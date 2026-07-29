e6fd60adfd952430c64a7d72b9f5acd29887a293
dawid2077 <praktykant31415@gmail.com>
2026-07-29 20:30:51 +0200

    Add: validate-parentheses - submission-11

---
+++ Data Structures & Algorithms/validate-parentheses/submission-11.py
@@ -0,0 +1,32 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        toClose=[]
+        left=("[","{","(")
+        right=("]","}",")")
+        reverse = {
+            '[': ']',
+            ']': '[',
+            '(': ')',
+            ')': '(',
+            '{': '}',
+            '}': '{',
+        }
+        if len(s)==1:
+            return False
+        for i in s:
+            if i in left:
+                toClose.append(i)
+            if i in right:
+                try:
+                    if reverse[i]==toClose[-1]:
+                        toClose.pop()
+                    else:
+                        return False
+                except:
+                    return False
+        if len(toClose)==0:
+            return True
+        else:
+            return False
+
+
